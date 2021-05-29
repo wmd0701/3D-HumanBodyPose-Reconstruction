adc81b346480677af3fc96b3e75c995a3d904d59
maisseal <maisseal@ethz.ch>
2021-05-29 17:30:40 +0200

    Add function that load pretrained resnet

---
+++ codebase/regressor/resnet.py
@@ -0,0 +1,9 @@
+from torchvision import models
+
+def load_resnet(freeze=True):
+    resnet50 = models.resnet50(pretrained=True)
+    
+    for param in resnet50.parameters():
+        param.requires_grad = freeze
+    
+    return resnet50
