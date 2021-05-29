aa9feb5350dc8977b4af2896ed214da0a41ec7ea
maisseal <maisseal@ethz.ch>
2021-05-29 17:42:11 +0200

    requires grad should be False if freeze is True

--- codebase/regressor/resnet.py
+++ codebase/regressor/resnet.py
@@ -4,6 +4,6 @@ def load_resnet(freeze=True):
     resnet50 = models.resnet50(pretrained=True)
     
     for param in resnet50.parameters():
-        param.requires_grad = freeze
+        param.requires_grad = not freeze
     
     return resnet50
