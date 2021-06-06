7623fb3aa52b3e3aa8dc5d494457172faac45f11
Aygul Zagidullina <azagidull@student.ethz.ch>
2021-06-06 14:21:44 +0000

    Replace backbone.py

--- codebase/regressor/backbone.py
+++ codebase/regressor/backbone.py
@@ -7,6 +7,10 @@ def load_backbone(backbone):
         model = load_resnet50()
         model.fc = nn.Identity()
         return model
+    elif backbone == 'resnet152':
+        model = load_resnet152()
+        model.fc = nn.Identity()
+        return model    
     else:
         raise Exception(f'Backbone \'{backbone}\' is not supported.')
 
@@ -18,3 +22,13 @@ def load_resnet50(freeze=True):
         param.requires_grad = (not freeze)
     
     return resnet50
+
+# Add ResNet-152. We freeze all the network except the final layer. requires_grad == False
+
+def load_resnet152(freeze=True):
+    resnet152 = models.resnet152(pretrained=True)
+    
+    for param in resnet152.parameters():
+        param.requires_grad = (not freeze)
+    
+    return resnet152    
